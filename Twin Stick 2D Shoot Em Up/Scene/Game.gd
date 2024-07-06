181856afacc7335c0611e43a0eacf68c2376236a
nfsmith <nickfsmith96@gmail.com>
2024-07-06 13:06:18 -0400

    Episode 12 Interpolation

--- Twin Stick 2D Shoot Em Up/Scene/Game.gd
+++ Twin Stick 2D Shoot Em Up/Scene/Game.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var player_spawn = preload("res://Scene/characterTemplate.tscn")
 var last_world_state = 0
+var world_state_buffer = []
+const interpolation_offset = .1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,21 +31,27 @@ func _DespawnPlayer(player_id):
 	get_node("Main Scene/OtherPlayers/"+str(player_id)).queue_free()
 
 func UpdateWorldState(world_state):
-	# Buffer
-	# Interpolation
-	# Extrapolation
-	# Rubber Banding
 	if world_state["T"] > last_world_state:
 		last_world_state = world_state["T"]
-		world_state.erase("T")
-		world_state.erase(multiplayer.get_remote_sender_id())
-		for player in world_state.keys():
+		world_state_buffer.append(world_state)
+
+func _physics_process(delta):
+	var render_time = Time.get_unix_time_from_system() - interpolation_offset
+	if world_state_buffer.size() > 1:
+		while world_state_buffer.size() > 2 and render_time > world_state_buffer[1]["T"]:
+			world_state_buffer.remove_at(0)
+		var interpolation_factor = float(render_time - world_state_buffer[0]["T"])/float(world_state_buffer[1]["T"] - world_state_buffer[0]["T"])
+		for player in world_state_buffer[1].keys():
+			if str(player) == "T":
+				continue
+			if player == multiplayer.get_unique_id():
+				continue
+			if not world_state_buffer[0].has(player):
+				continue
 			if get_node("Main Scene/OtherPlayers").has_node(str(player)):
-				get_node("Main Scene/OtherPlayers/"+str(player)).MovePlayer(world_state[player]["P"])
+				var new_position = lerp(world_state_buffer[0][player]["P"], world_state_buffer[1][player]["P"], interpolation_factor)
+				get_node("Main Scene/OtherPlayers/"+str(player)).MovePlayer(new_position)
 			else:
 				print("spawning player")
-				SpawnNewPlayer(player, world_state[player]["P"])
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+				SpawnNewPlayer(player, world_state_buffer[1][player]["P"])
+				
